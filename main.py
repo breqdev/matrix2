7ddcc0fa5e0e9cb7223af0214373f6b26696946e
Ava Silver <avapsilver@gmail.com>
2025-02-12 22:28:35 -0500

    add metrics

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ from PIL import Image
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
 
 from dotenv import load_dotenv
+from datadog import initialize, statsd
 
 load_dotenv()
+initialize(statsd_disable_buffering=False)
 
 from matrix.bluebikes import get_image_bluebikes
 from matrix.fish import get_image_fish
@@ -26,6 +28,7 @@ matrix_options.drop_privileges = False
 matrix = RGBMatrix(options=matrix_options)
 
 logger = logging.getLogger(__name__)
+IMAGE_TYPES = ["mbta", "spotify", "weather", "bluebikes"]
 
 try:
     tick = 0
@@ -34,6 +37,8 @@ try:
 
         image: Image.Image | None = None
 
+        img_type_tag = "image:" + IMAGE_TYPES[tick % 4]
+        t0 = time.time()
         try:
             if datetime.datetime.now().strftime("%H:%M") in ["11:11", "23:11"]:
                 image = get_image_fish()
@@ -48,8 +53,10 @@ try:
                     case 3:
                         image = get_image_bluebikes()
                 # image = get_image_transit(tick % 2)
+            statsd.gauge("matrix.load_seconds", time.time() - t0, tags=[img_type_tag])
         except Exception as e:
             logger.exception("Exception during rendering", exc_info=e)
+            statsd.increment("matrix.exception", tags=[img_type_tag])
             image = get_image_no_connection()
 
         if not image:
