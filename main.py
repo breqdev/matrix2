c2e4c4d8bd19905925df18148456b94bb710dd36
Ava Silver <avapsilver@gmail.com>
2025-02-12 23:46:08 -0500

    simplify

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from collections.abc import Callable
 import time
 import datetime
 import logging
+from itertools import cycle
 
-from PIL import Image
+from PIL.Image import Image
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
 
 from dotenv import load_dotenv
@@ -28,35 +30,29 @@ matrix_options.drop_privileges = False
 matrix = RGBMatrix(options=matrix_options)
 
 logger = logging.getLogger(__name__)
-IMAGE_TYPES = ["mbta", "spotify", "weather", "bluebikes"]
+
+IMAGE_GENERATORS: dict[str, Callable[[], Image | None]] = {
+    "mbta": get_image_mbta,
+    "spotify": get_image_spotify,
+    "weather": get_image_weather,
+    "bluebikes": get_image_bluebikes,
+}
+
+
+def is_eleven_eleven() -> bool:
+    return datetime.datetime.now().strftime("%H:%M") in ["11:11", "23:11"]
+
 
 try:
-    tick = 0
-    while True:
-        tick += 1
-
-        image: Image.Image | None = None
-
-        img_type_tag = "image:" + IMAGE_TYPES[tick % 4]
+    for image_type, get_image in cycle(IMAGE_GENERATORS.items()):
         t0 = time.time()
+        tags = [f"image:{image_type}"]
         try:
-            if datetime.datetime.now().strftime("%H:%M") in ["11:11", "23:11"]:
-                image = get_image_fish()
-            else:
-                match tick % 4:
-                    case 0:
-                        image = get_image_mbta()
-                    case 1:
-                        image = get_image_spotify()
-                    case 2:
-                        image = get_image_weather()
-                    case 3:
-                        image = get_image_bluebikes()
-                # image = get_image_transit(tick % 2)
-            statsd.gauge("matrix.load_seconds", time.time() - t0, tags=[img_type_tag])
+            image = get_image_fish() if is_eleven_eleven() else get_image()
+            statsd.gauge("matrix.load_seconds", time.time() - t0, tags=tags)
         except Exception as e:
             logger.exception("Exception during rendering", exc_info=e)
-            statsd.increment("matrix.exception", tags=[img_type_tag])
+            statsd.increment("matrix.exception", tags=tags)
             image = get_image_no_connection()
 
         if not image:
