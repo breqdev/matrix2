9dee75359c7b6288fbdfa113e16c5b468b220184
Ava Silver <avapsilver@gmail.com>
2025-03-01 23:13:14 -0500

    initial progress

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
+# stdlib
 from collections.abc import Callable
+from itertools import cycle
 import time
 import datetime
 import logging
-from itertools import cycle
 
+# 3p
 from PIL.Image import Image
-from rgbmatrix import RGBMatrix, RGBMatrixOptions
-
+from rgbmatrix import RGBMatrix, RGBMatrixOptions  # type: ignore
+from gpiozero import RotaryEncoder, Button
 from dotenv import load_dotenv
 from datadog import initialize, statsd
 from json_log_formatter import JSONFormatter
 
-load_dotenv()
-initialize(statsd_disable_buffering=False)
-
+# project
 from matrix.bluebikes import get_image_bluebikes
 from matrix.fish import get_image_fish
 from matrix.mbta import get_image_mbta
 from matrix.spotify import get_image_spotify
 from matrix.weather import get_image_weather
 from matrix.no_connection import get_image_no_connection
-from matrix.transit import get_image_transit
+from matrix.menu import draw_menu
+
+load_dotenv()
+initialize(statsd_disable_buffering=False)
 
 
 matrix_options = RGBMatrixOptions()
@@ -29,6 +32,9 @@ matrix_options.cols = 64
 matrix_options.drop_privileges = False
 
 matrix = RGBMatrix(options=matrix_options)
+
+dial = RotaryEncoder(8, 7, max_steps=1024, wrap=True)
+button = Button(25)
 
 logger = logging.getLogger(__name__)
 handler = logging.FileHandler(filename="/var/log/matrix2.log")
@@ -41,6 +47,7 @@ IMAGE_GENERATORS: dict[str, Callable[[], Image | None]] = {
     "weather": get_image_weather,
     "bluebikes": get_image_bluebikes,
 }
+GENERATORS = cycle(IMAGE_GENERATORS.items())
 
 
 def is_eleven_eleven() -> bool:
@@ -48,28 +55,54 @@ def is_eleven_eleven() -> bool:
     return now.minute == 11 and now.hour in {11, 23}
 
 
+menu = False
+
+
 try:
-    for image_type, get_image in cycle(IMAGE_GENERATORS.items()):
-        t0 = time.time()
-        try:
-            if is_eleven_eleven():
-                image = get_image_fish()
-                image_type = "fish"
-            else:
-                image = get_image()
-            statsd.gauge(
-                "matrix.load_seconds", time.time() - t0, tags=[f"image:{image_type}"]
-            )
-        except Exception as e:
-            logger.exception("Exception during rendering", exc_info=e)
-            statsd.increment("matrix.exception", tags=[f"image:{image_type}"])
-            image = get_image_no_connection()
+    while True:
+        if menu:
+            dial.value = 0
+            image = draw_menu(dial)
+            matrix.SetImage(image.convert("RGB"))
 
-        if not image:
-            continue
+            button.wait_for_active(timeout=0.1)
+            if button.is_active:
+                button.wait_for_inactive()
+                print("Moving to display page")
+                state = "display"
+        else:
+            image_type, get_image = next(GENERATORS)
 
-        matrix.SetImage(image.convert("RGB"))
-        time.sleep(5)
+            t0 = time.time()
+            try:
+                if is_eleven_eleven():
+                    image: Image | None = get_image_fish()
+                    image_type = "fish"
+                else:
+                    image = get_image()
+                statsd.gauge(
+                    "matrix.load_seconds",
+                    time.time() - t0,
+                    tags=[f"image:{image_type}"],
+                )
+            except Exception as e:
+                logger.exception("Exception during rendering", exc_info=e)
+                statsd.increment("matrix.exception", tags=[f"image:{image_type}"])
+                image = get_image_no_connection()
+
+            if not image:
+                continue
+
+            matrix.SetImage(image.convert("RGB"))
+
+            # move on once we're ready for the next image,
+            # or if the user tries to enter the menu
+            button.wait_for_active(timeout=5.0)
+            if button.is_active:
+                button.wait_for_inactive()
+                print("Moving to menu page")
+                state = "menu"
+
 
 finally:
     matrix.Clear()
---
+++ matrix/menu.py
@@ -0,0 +1,48 @@
+from PIL import Image, ImageDraw
+
+from matrix.fonts import font
+
+
+OPTIONS = [
+    "Brightness",
+    "Scenes",
+    "Network",
+    "Power Off",
+    "Home",
+]
+
+SELECTED_OPTION = 0
+
+
+def handle_rotation_clockwise():
+    global SELECTED_OPTION
+    SELECTED_OPTION = (SELECTED_OPTION + 1) % len(OPTIONS)
+
+
+def handle_rotation_counter_clockwise():
+    global SELECTED_OPTION
+    SELECTED_OPTION = (SELECTED_OPTION - 1 + len(OPTIONS)) % len(OPTIONS)
+
+
+def draw_menu(encoder) -> Image.Image:
+    global SELECTED_OPTION
+
+    image = Image.new("RGB", (64, 64))
+    draw = ImageDraw.Draw(image)
+
+    encoder.when_rotated_clockwise = handle_rotation_clockwise
+    encoder.when_rotated_counter_clockwise = handle_rotation_counter_clockwise
+
+    draw.text((0, 1), text="  Settings   ", font=font, fill="#00ffff")
+
+    for i, name in enumerate(OPTIONS):
+        if SELECTED_OPTION == i:
+            color = "#ffffff"
+        else:
+            color = "#888888"
+        draw.text((1, 12 + 10 * i), text=">", font=font, fill=color)
+        draw.text((12, 12 + 10 * i), text=name, font=font, fill=color)
+
+    draw.rectangle((0, 10 + 10 * SELECTED_OPTION, 63, 10 + 10 * SELECTED_OPTION + 9))
+
+    return image
