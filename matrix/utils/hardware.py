2df3672995ede1f48434ca3cfb312337325e8fe3
Ava Silver <ava@avasilver.dev>
2026-07-12 17:40:21 -0400

    remove redundant default

--- matrix/utils/hardware.py
+++ matrix/utils/hardware.py
@@ -10,14 +10,14 @@ class Hardware:
     button: Button
 
     def __init__(self):
-        config = get_config()
+        panel = get_config().panel
         matrix_options = RGBMatrixOptions()
-        matrix_options.cols, matrix_options.rows = config.panel.size.value
+        matrix_options.cols, matrix_options.rows = panel.size.value
         matrix_options.drop_privileges = False
         matrix_options.hardware_mapping = "adafruit-hat-pwm"
 
         self.matrix = RGBMatrix(options=matrix_options)
-        self.matrix.brightness = config.panel.brightness or 60
+        self.matrix.brightness = panel.brightness
 
         self.dial = RotaryEncoder(7, 19, max_steps=1024, wrap=True, bounce_time=0.1)
         self.button = Button(25, bounce_time=0.1)
